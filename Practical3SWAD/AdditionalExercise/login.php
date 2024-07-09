39e72e2ebcc07a90b2ce8fea352b134904309f4c
GY523 <127575274+GY523@users.noreply.github.com>
2024-07-09 14:00:00 +0800

    Add files via upload

---
+++ Practical3SWAD/AdditionalExercise/login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>User Login</title>
+</head>
+<body>
+<?php
+require('database.php');
+if (isset($_POST['email'])){
+$email = stripslashes($_REQUEST['email']);
+$email = mysqli_real_escape_string($con,$email);
+$password = stripslashes($_REQUEST['password']);
+$password = mysqli_real_escape_string($con,$password);
+$query = "SELECT *
+FROM `exercise3`
+WHERE email='$email'
+AND password='".md5($password)."'"
+;
+$result = mysqli_query($con,$query) or die(mysqli_error($con));
+$rows = mysqli_num_rows($result);
+if($rows==1){
+$_SESSION['email'] = $email;
+header("Location: index.php");
+exit();
+}else{
+echo "<div class='form'>
+<h3>email/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+}
+}else{
+?>
+<div class="form">
+<h1>User Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="email" placeholder="Email" required /><br>
+<input type="password" name="password" placeholder="Password" required /><br>
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+</div>
+<?php } ?>
+</body>
+</html>
---
+++ Practical3SWAD/users.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 09, 2024 at 07:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `reg_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `reg_date`) VALUES
+(1, 'GRAY', 'funnychwa523@gmail.com', '735dbe14ca7c12da87760c43c8f57dc1', '2024-07-09 06:46:08'),
+(2, 'chwa', 'funnychwa523@gmail.com', '4297f44b13955235245b2497399d7a93', '2024-07-09 07:27:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
