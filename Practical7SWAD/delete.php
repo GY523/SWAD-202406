a977de5dc71038f8fc63aee3343337562b02b1c6
GY523 <127575274+GY523@users.noreply.github.com>
2024-08-27 14:31:15 +0800

    test for editing

--- Practical7SWAD/delete.php
+++ Practical7SWAD/delete.php
@@ -3,6 +3,8 @@ require('database.php');
 $id=$_GET['id'];
 $query = "DELETE FROM products WHERE id=$id";
 $result = mysqli_query($con,$query) or die ( mysqli_error($con));
+echo "hahahaa";
 header("Location: view.php");
 exit();
+
 ?>
