fb7486e9c30854f26e1922439a36504f50d2fb6f
GY523 <127575274+GY523@users.noreply.github.com>
2024-09-26 23:49:37 +0800

    Delete Practical7SWAD/SWADPractical9 directory

--- Practical7SWAD/SWADPractical9/exercise2/greet.php
+++
@@ -1,9 +0,0 @@
-<?php
-if($_SERVER['REQUEST_METHOD']=="POST"){
-    if(isset($_POST['action']) && $_POST['action']==="submitName"){
-        //get the name to construct the greeting msg and echo back the msg
-        $name= $_POST['name'] ?? "";
-        echo "<span> Hi $name,Welcome to UTAR fyp portal </span>";
-    }
-}
-?>
--- Practical7SWAD/SWADPractical9/exercise2/script.js
+++
@@ -1,9 +0,0 @@
-function submitName(){
-    var username=$("#inputName").val();
-    //send the post request with the username
-    $.post('greet.php',{action:"submitName", name:username}, function(response){
-        $("#greetingsContainer").html(response);
-    })
-}
-
-$(document).ready();
